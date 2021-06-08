bff78224922ed233201084fc859f5b277861d1d1
Doug Barr <douglas.barr01@gmail.com>
2021-06-08 16:05:48 +0100

    Added mapbox and markers to the attempt show view, pulled geocoded co-ordinates through - worked fine

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,3 +1,13 @@
 class AttemptsController < ApplicationController
-  
+  def show
+    @attempt = Attempt.find(params[:id])
+    @cycle_route = @attempt.cycle_route
+
+    @markers = [
+      {
+        lat: @cycle_route.latitude,
+        lng: @cycle_route.longitude
+      }
+    ]
+  end
 end
