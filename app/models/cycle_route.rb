e4c37b20b7fe8e8b3b51260e3cd36fff40abc939
Timothy Martin <tpmartin7@gmail.com>
2021-06-08 12:05:19 +0100

    1st seed

--- app/models/cycle_route.rb
+++ app/models/cycle_route.rb
@@ -1,5 +1,7 @@
 class CycleRoute < ApplicationRecord
   belongs_to :user
-  has_many :attempts, :reviews, :route_tags
+  has_many :attempts
+  has_many :reviews
+  has_many :route_tags
   has_many :tags, through: :route_tags
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+admin = User.create(email: 'admin', password: '123456')
+
+CycleRoute.create(
+    name: "Victoria Park",
+    start_point: "London E2 9JW",
+    end_point: "London E20 1JB",
+    user: admin
+  )
