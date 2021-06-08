e714e4ebed248ffad9913cd1ee98617b2ee64f58
Kieryn Moore <kierynmoore@hotmail.com>
2021-06-08 14:24:54 +0100

    created button on the cycle_routes show page that links to the attempt show page passing the cycle_route information

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cycle_routes
+
+  resources :cycle_routes, only: [:index, :show] do
+    resources :attempts, only: [:show]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
