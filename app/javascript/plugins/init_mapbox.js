2bc66cd80d7622bd04a952922754a8e1859e262c
Doug Barr <douglas.barr01@gmail.com>
2021-06-09 16:06:50 +0100

    Added mapbox directions functionality, map now drops a start and end pin, draws a cycle route and displays directions and time

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,22 +9,167 @@ const initMapbox = () => {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
-  });
-    const markers = JSON.parse(mapElement.dataset.markers);
+    });
+    if (mapElement.dataset.markers) {
+      const markers = JSON.parse(mapElement.dataset.markers);
       markers.forEach((marker) => {
         new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
-  });
-  if (mapElement) {
-    const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
-    fitMapToMarkers(map, markers);
-}
-}
+      });
+      if (mapElement) {
+        const fitMapToMarkers = (map, markers) => {
+          const bounds = new mapboxgl.LngLatBounds();
+            markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+            map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+        };
+        fitMapToMarkers(map, markers);
+      }
+
+      // initialize the map canvas to interact with later
+      var canvas = map.getCanvasContainer();
+
+      // an arbitrary start will always be the same
+      // only the end or destination will change
+      var start = [markers[0].lng, markers[0].lat];
+      function getRoute(end) {
+        // make a directions request using cycling profile
+        // an arbitrary start will always be the same
+        // only the end or destination will change
+        var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+      
+        // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+        var req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        req.onload = function() {
+          var json = JSON.parse(req.response);
+          var data = json.routes[0];
+          var route = data.geometry.coordinates;
+          var geojson = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: route
+            }
+          };
+          // if the route already exists on the map, reset it using setData
+          if (map.getSource('route')) {
+            map.getSource('route').setData(geojson);
+          } else { // otherwise, make a new request
+            map.addLayer({
+              id: 'route',
+              type: 'line',
+              source: {
+                type: 'geojson',
+                data: {
+                  type: 'Feature',
+                  properties: {},
+                  geometry: {
+                    type: 'LineString',
+                    coordinates: geojson
+                  }
+                }
+              },
+              layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+              },
+              paint: {
+                'line-color': '#3887be',
+                'line-width': 5,
+                'line-opacity': 0.75
+              }
+            });
+          }
+          // add turn instructions here at the end
+          var instructions = document.getElementById('instructions');
+          var steps = data.legs[0].steps;
+          var tripInstructions = [];
+            for (var i = 0; i < steps.length; i++) {
+              tripInstructions.push('<br><li>' + steps[i].maneuver.instruction) + '</li>';
+              instructions.innerHTML = '<br><span class="duration">Trip duration: ' + Math.floor(data.duration / 60) + ' min 🚴 </span>' + tripInstructions;
+            }
+
+        };
+        req.send();
+      }
+      
+      map.on('load', function() {
+        // make an initial directions request that
+        // starts and ends at the same location
+        getRoute(start);
+      
+        // Add starting point to the map
+        map.addLayer({
+          id: 'point',
+          type: 'circle',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [{
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'Point',
+                  coordinates: start
+                }
+              }
+              ]
+            }
+          },
+          paint: {
+            'circle-radius': 10,
+            'circle-color': '#3887be'
+          }
+        });
+        let endPoint = [markers[1].lng, markers[1].lat]
+        var end = {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: endPoint
+            }
+          }
+          ]
+        };
+        if (map.getLayer('end')) {
+          map.getSource('end').setData(end);
+        } else {
+          map.addLayer({
+            id: 'end',
+            type: 'circle',
+            source: {
+              type: 'geojson',
+              data: {
+                type: 'FeatureCollection',
+                features: [{
+                  type: 'Feature',
+                  properties: {},
+                  geometry: {
+                    type: 'Point',
+                    coordinates: endPoint
+                  }
+                }]
+              }
+            },
+            paint: {
+              'circle-radius': 10,
+              'circle-color': '#f30'
+            }
+          });
+        }
+        
+        getRoute(endPoint);
+        // this is where the code from the next step will go
+         
+      });
+    }
+  }
 };
 
 export { initMapbox };
