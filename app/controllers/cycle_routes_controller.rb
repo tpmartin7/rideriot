52a7aefa41670617b93ef9177917118feea11d5c
Kieryn Moore <kierynmoore@hotmail.com>
2021-06-09 13:00:47 +0100

    first attempt at styling the users dashboard, still need to integrate has_friendship gem to create user friends and make a leaderboard of the total points they all have

--- app/controllers/cycle_routes_controller.rb
+++ app/controllers/cycle_routes_controller.rb
@@ -1,4 +1,6 @@
 class CycleRoutesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
   def index
     @seed_images = {
       'Victoria Park' => "rideriot/vicPark_cerwb5.png",
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :attempts, only: [:show]
   end
 
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
