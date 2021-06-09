592db64c0c5f76e834b474ab83dd86eb8978116c
Timothy Martin <tpmartin7@gmail.com>
2021-06-09 14:05:12 +0100

    Hacky fix to go back to index from show

--- app/controllers/cycle_routes_controller.rb
+++ app/controllers/cycle_routes_controller.rb
@@ -6,10 +6,22 @@ class CycleRoutesController < ApplicationController
       'CS1' => "rideriot/cs1_vvgyjl.png",
       'Tower of London' => "rideriot/tower_u3rjsc.png"
     }
-    @cycle_routes = CycleRoute.near(params[:address_form][:address], 5)
+    # THIS IS A TEMPORARY FIX ... need to remember the address if returning
+    # to the index from show
+    if params[:address_form].nil?
+      @cycle_routes = CycleRoute.near("138 Kingsland Rd, London E2 8DY", 5)
+    else
+      @cycle_routes = CycleRoute.near(params[:address_form][:address], 5)
+    end
   end
 
   def show
+    @seed_images = {
+      'Victoria Park' => "rideriot/vicPark_cerwb5.png",
+      'Regents Canal' => "rideriot/regCanal_jj3i9r.png",
+      'CS1' => "rideriot/cs1_vvgyjl.png",
+      'Tower of London' => "rideriot/tower_u3rjsc.png"
+    }
     @cycle_route = CycleRoute.find(params[:id])
   end
 end
