25d7072d3f9f1f1f45172470af2de4656e79ac9e
Kieryn Moore <kierynmoore@hotmail.com>
2021-06-10 15:56:01 +0100

    reviews table created, reviews displayed on cycle routes show page along with average rating in stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :cycle_route
   belongs_to :user
+  validates :rating, length: { maximum: 5, too_long: "5 stars is the maximum allowed" }
+  validates :rating, presence: true
+  validates :comment, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :cycle_routes, only: [:index, :show] do
     resources :attempts, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
   resources :attempts, only: [:show]
   resources :users, only: [:show]
