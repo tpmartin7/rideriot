6073a119aaf419ae5287c6eb34cf26873fbe690c
Doug Barr <douglas.barr01@gmail.com>
2021-06-10 18:03:57 +0100

    styled homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    attempt_show
+  end
 
+  private 
+  def attempt_show
+    @disable_nav = true
   end
 
 end
