bc83125c8bf115bbc9c4cd79e54eb2d3688504a9
Timothy Martin <tpmartin7@gmail.com>
2021-06-15 11:13:20 +0100

    Added default values for new users: distance cycled = 0, routes completed = 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_initialize :init
+
+  def init
+    self.distance_cycled ||= 0.0
+    self.routes_completed ||= 0
+  end
 end
